f45a4a9919636ced73cfa3bd7071b85f9495f9a0
Masa08 <ushimasarug08@gmail.com>
2018-07-22 11:47:17 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/help', to: 'static_pages#help', as: 'help'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
